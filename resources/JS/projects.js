6a7164d4fdc542a6ea40af188fb876bd1fee4624
iftesam <iftesamnabi@gmail.com>
2022-11-27 20:11:40 -0600

    searchbar

--- resources/JS/projects.js
+++ resources/JS/projects.js
@@ -9,11 +9,68 @@ const search = () => {
 
     if (match) {
       let textvalue = match.textContent;
+
       if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
-        product[i].getElementsByClassName.display = "";
+        product[i].style.display = "";
       } else {
         product[i].style.display = "none";
       }
     }
   }
 };
+
+document.getElementById("fwd").addEventListener("click", function () {
+  var text = document.getElementById("search-item");
+  text.value = "Front-End Web Development";
+  text.focus();
+  text.click();
+});
+
+document.getElementById("lc").addEventListener("click", function () {
+  var text = document.getElementById("search-item");
+  text.value = "LeetCode";
+  text.focus();
+  text.click();
+});
+
+document.getElementById("gd").addEventListener("click", function () {
+  var text = document.getElementById("search-item");
+  text.value = "Game-Development";
+  text.focus();
+  text.click();
+});
+
+document.getElementById("js").addEventListener("click", function () {
+  var text = document.getElementById("search-item");
+  text.value = "JavaScript";
+  text.focus();
+  text.click();
+});
+
+document.getElementById("python").addEventListener("click", function () {
+  var text = document.getElementById("search-item");
+  text.value = "Python";
+  text.focus();
+  text.click();
+});
+
+document.getElementById("ios").addEventListener("click", function () {
+  var text = document.getElementById("search-item");
+  text.value = "IOS Development";
+  text.focus();
+  text.click();
+});
+
+document.getElementById("cs").addEventListener("click", function () {
+  var text = document.getElementById("search-item");
+  text.value = "Cool-Stuffs";
+  text.focus();
+  text.click();
+});
+
+document.getElementById("all").addEventListener("click", function () {
+  var text = document.getElementById("search-item");
+  text.value = "See-All";
+  text.focus();
+  text.click();
+});
