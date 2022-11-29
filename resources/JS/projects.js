fd4937f5e9566d256ed212bd13d827d8a9740739
iftesam <iftesamnabi@gmail.com>
2022-11-29 03:33:36 -0600

    updated searchbox

--- resources/JS/projects.js
+++ resources/JS/projects.js
@@ -1,19 +1,19 @@
 const search = () => {
   const searchbox = document.getElementById("search-item").value.toUpperCase();
-  const storeitems = document.getElementById("flex-grid");
-  const product = document.querySelectorAll(".col");
-  const pname = storeitems.getElementsByTagName("h6");
+  const projects = document.getElementById("flex-grid");
+  const eachproject = document.querySelectorAll(".col");
+  const tags = projects.getElementsByTagName("h6");
 
-  for (var i = 0; i < pname.length; i++) {
-    let match = product[i].getElementsByTagName("h6")[0];
+  for (var i = 0; i < tags.length; i++) {
+    let match = eachproject[i].getElementsByTagName("h6")[0];
 
     if (match) {
       let textvalue = match.textContent;
 
       if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
-        product[i].style.display = "";
+        eachproject[i].style.display = "";
       } else {
-        product[i].style.display = "none";
+        eachproject[i].style.display = "none";
       }
     }
   }
