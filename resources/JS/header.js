8f9623df1a614751d1599355bb2519512838cb07
iftesam <iftesamnabi@gmail.com>
2022-12-06 02:42:02 -0600

    Update header.js

--- resources/JS/header.js
+++ resources/JS/header.js
@@ -2,6 +2,8 @@ document.getElementById("inacodeLink").href = "http://www.inacode.io";
 document.getElementById("backward").href = "";
 document.getElementById("videobanners").href =
   "https://www.youtube.com/@inacode9173";
+document.getElementById("videobanners").target = "_blank";
+
 document.getElementById("portfolio").addEventListener("click", function () {
   let nav = document.querySelectorAll(".navicon");
   nav[0].style.display = "inline-block";
